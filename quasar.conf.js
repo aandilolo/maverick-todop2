695b5f1cb6287c89e1ef43ea0b05ffd020a82443
aandilolo <aandilolo@unomaha.edu>
2020-11-29 19:07:35 -0600

    commit2

--- quasar.conf.js
+++ quasar.conf.js
@@ -175,6 +175,8 @@ module.exports = function (/* ctx */) {
       packager: {
         // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
 
+        platform: 'darwin'
+
         // OS X / Mac App Store
         // appBundleId: '',
         // appCategoryType: '',
